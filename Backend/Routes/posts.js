bdbc5d6041ce316fadbe15d6825ee06c8105a8b0
jafarpathan <shahrukhpathan54@gmail.com>
2020-05-04 12:47:04 +0530

    convert TDF to reactive Form

--- Backend/Routes/posts.js
+++ Backend/Routes/posts.js
@@ -37,12 +37,23 @@ router.get("", (req, res, next)=>{
   })
 });
 
+router.get("/:id",(req, res, next) => {
+  Post.findById(req.params.id).then(post=>{
+    if(post){
+      console.log(post)
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({message:"Post not found"})
+    }
+  })
+})
 
 router.delete("/:id", (req, res, next)=>{
-  console.log(req.params._id);
+  console.log(req.params.id);
   Post.deleteOne({_id: req.params.id})
     .then((result)=>{
       console.log(result)
+
     res.status(200).json({message: "deleted"});
 
     })
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import { Post } from '../post.module';
 import { PostService } from '../post.service';
@@ -16,31 +16,42 @@ export class PostCreateComponent implements OnInit{
   mode = 'create';
   postId: string;
   post;
+  form: FormGroup;
 
   constructor(private postService: PostService, public route: ActivatedRoute){}
 
   ngOnInit(){
+    this.form = new FormGroup({
+      title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
+      content: new FormControl(null, {validators: [Validators.required]})
+    })
     this.route.paramMap.subscribe((paramMap:ParamMap)=>{
       if(paramMap.has('postId')){
         this.mode='edit';
         this.postId = paramMap.get('postId');
-        this.post = this.postService.getPost(this.postId);
-
+        this.postService.getPost(this.postId).subscribe(postData => {
+          console.log("tuka")
+          this.post = {id: postData._id, title: postData.title, content: postData.content}
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content
+          })
+        });
       }else{
         this.mode= 'create';
       }
     })
   }
 
-  onAddPost(form:NgForm){
-    if(form.invalid){
+  onAddPost(){
+    if(this.form.invalid){
       return
     }
     if(this.mode === 'create'){
-    this.postService.addPost(form.value.title, form.value.content)
+    this.postService.addPost(this.form.value.title, this.form.value.content)
     }else{
-      this.postService.updatePost(this.postId, form.value.title, form.value.content)
+      this.postService.updatePost(this.postId, this.form.value.title, this.form.value.content)
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
