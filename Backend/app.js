9d4fae5639ad7f2c5c098235861b4780aa72ea5b
jafarpathan <shahrukhpathan54@gmail.com>
2020-06-08 01:38:27 +0530

    image upload

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const mongoose = require('mongoose');
 const postRoutes = require('./Routes/posts')
 
@@ -16,6 +17,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}))
+app.use("/images", express.static(path.join("Backend/images")))
+// app.use((req, res, next)=>{
+//   console.log(req)
+//   next()
+// })
+
 app.use((req, res, next)=>{
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
